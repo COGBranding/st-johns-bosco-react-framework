6d8820155d7de1acd07e75885d1aabfa0b2e906e
Blake Zajac <contact@blakezajac.com>
2023-07-21 16:36:16 +1000

    Push latest test.js changes

--- test.js
+++ test.js
@@ -4,37 +4,36 @@ outerPieces.forEach((piece, index) => {
     piece.setAttribute("id", `curve-${index + 1}`);
 });
 
-const outerPiecesText = document.querySelectorAll(
-    ".pie-chart--outer svg g g:last-child g"
-);
+// const outerPiecesText = document.querySelectorAll(
+//     ".pie-chart--outer svg g g:last-child g"
+// );
 
-outerPiecesText.forEach((layer, index) => {
+outerPieces.forEach((layer, index) => {
     // Get the parent path from Nivo's generated arc
     const parentPath = document.querySelector(`#curve-${index + 1}`);
 
     // Get the d value from the parent path
     let d = parentPath.getAttribute("d");
 
-    // Create a new path
-    const newPath = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "path"
-    );
+    // // Create a new path
+    // const newPath = document.createElementNS(
+    //     "http://www.w3.org/2000/svg",
+    //     "path"
+    // );
 
     // Set the new path d attribute to the parentPath d attribute
-    newPath.setAttribute("d", d);
+    // newPath.setAttribute("d", d);
 
     // Append the new path inside the existing group
-    const text = layer.querySelector("text");
+    // const text = layer.querySelector("text");
+    // const textHtml = text.innerHTML;
 
-    // Store the innerHTML of the original text element in a variable
-    const originalInnerHTML = text.innerHTML;
-
-    // Set the innerHTML of the original text element to empty
-    text.innerHTML = "";
-
-    // Append the original text element to the new path
-    newPath.appendChild(text);
+    const newText = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "text"
+    );
+    newText.setAttribute("text-anchor", "middle");
+    newText.setAttribute("dominant-baseline", "central");
 
     const svgTextPath = document.createElementNS(
         "http://www.w3.org/2000/svg",
@@ -47,17 +46,30 @@ outerPiecesText.forEach((layer, index) => {
         "xlink:href",
         `#curve-${index + 1}`
     );
+    svgTextPath.innerHTML = "Test";
 
-    // Create a new text element inside the new path
-    const newText = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-    );
-    svgTextPath.appendChild(newText);
+    layer.appendChild(newText);
+    newText.appendChild(svgTextPath);
+    // text.remove();
 
-    // Set the innerHTML of the new text element to the stored innerHTML
-    newText.innerHTML = originalInnerHTML;
+    // // Store the innerHTML of the original text element in a variable
+    // const originalInnerHTML = text.innerHTML;
 
-    text.appendChild(svgTextPath);
-    console.log(svgTextPath);
+    // // Append the original text element to the new path
+    // newPath.appendChild(text);
+
+    // // Create a new text element inside the new path
+    // const newText = document.createElementNS(
+    //     "http://www.w3.org/2000/svg",
+    //     "text"
+    // );
+    // svgTextPath.appendChild(newText);
+
+    // // Set the innerHTML of the new svgTextPath element to the stored innerHTML
+    // svgTextPath.innerHTML = originalInnerHTML;
+
+    // // Set the innerHTML of the original text element to empty
+    // text.innerHTML = "";
+
+    // text.appendChild(svgTextPath);
 });
